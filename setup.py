d942909fd410d1134a3427ef3dfcf396183d1247
Philip <p208p2002@gmail.com>
2020-08-26 17:28:23 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,6 +24,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.5',
     install_requires = requirements
 )
