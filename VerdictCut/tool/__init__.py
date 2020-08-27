35e8f6ff8a4fca87ed45aa09ed4571ff3f7043c7
josiriser <josiriser@gmail.com>
2020-08-27 13:24:50 +0800

    modified name

--- VerdictCut/tool/__init__.py
+++ VerdictCut/tool/__init__.py
@@ -1,4 +1,4 @@
 from .normalize_position import normalize_position
 from .find_name_law import find_name_and_law
 from .get_units_list import get_units_list
-from .find_job_location import find_job_location
+from .find_job_location_position import find_job_location_position
